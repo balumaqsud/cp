66630fcaaa478b27228ead2adba07581f6faef53
balumaqsud <ollobergan008@mail.ru>
2026-09-04 16:09:56 +0500

    fix: the test controller fix

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace Src\Controller;
+namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class TestController {
+class TestController extends AbstractController {
 
     #[Route('/test')]
     public function index(): Response
